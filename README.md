bdeb2b2d288d1c8338bef610d5e1f6563c4a84ac
Ng Teng Ann <tengann.ng@staff.main.ntu.edu.sg>
2022-09-20 14:05:21 +0800

    Cleaned datasets

--- README.md
+++
@@ -1,3 +0,0 @@
-# IAV-Host PPI Database
-
-#### The IAV-Host PPI database can be accessed at https://iav-ppi.herokuapp.com/home
--- maindash.py
+++
@@ -1,5 +0,0 @@
-import os
-import dash
-import dash_bootstrap_components as dbc
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
